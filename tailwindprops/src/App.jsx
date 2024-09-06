1ecca1a467fb46c4e6f99ae02961d07ff7ea7551
ChandanaVaidya17 <chandanavaidya172002@gmail.com>
2024-09-06 23:52:18 +0530

    tailwind css and props with react

---
+++ tailwindprops/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
+    <Card channel='coding' />
+
+    {/* <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+  <img class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="https://images.pexels.com/photos/843226/pexels-photo-843226.jpeg?auto=compress&cs=tinysrgb&w=300" alt="" width="384" height="512" />
+  <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+    <blockquote>
+      <p class="text-lg font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p>
+    </blockquote>
+  </div>
+</figure> */}
+
+
+
+
+    </>
+  )
+}
+
+export default App
